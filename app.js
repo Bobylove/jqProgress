4fe22e940ce80d1229761c90cbd13d8250793250
Fred <delon.frederic.pro.job@gmail.com>
2017-08-05 00:13:19 +0200

    test function animate

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
 
-	$("#p1").click(function(){
-
-		$('#p2').fadeToggle(3000, function(){
-			$('#p3').toggle(3000, function(){
-				$('#p4').toggle(3000);
-			});
+	$("button").click(function(){
+		$('div').animate({left : '600px'}, 3000, function(){
+			$("div").fadeOut(2000);
 		});
 	});
+
 
 });
