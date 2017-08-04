8d88873f3aa5b607cbd1c3bd7b6053034ebb73f2
Fred <delon.frederic.pro.job@gmail.com>
2017-08-05 01:48:34 +0200

    animatedItem function

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ $(document).ready(function(){
 	var $mainMenuItems = $('#main-menu ul').children("li"),
 	totalMainMenuItems = $mainMenuItems.length,
 	openedIndex = -1,
+
+
 	init = function(){
+
 		$mainMenuItems.children(".images").click(function(){
 			var newIndex = $(this).parent().index(),
-			$item = $mainMenuItems.eq(newIndex),
-			$colorImage = $item.find(".color");
-
-			$colorImage.animate({left:'0px'}, 250);
-			$item.animate({width: '420px'},250);
+			$item = $mainMenuItems.eq(newIndex);
+			animateItem($item, true, 250);
 			openedIndex = newIndex;
 		});
+	},
+
+	animateItem = function($item, toOpen, speed){
+		var $colorImage = $item.find(".color"),
+		itemParam = toOpen ? {width: '420px'}: {width: '140px'},
+		colorImageParam = toOpen ? {left: '0px'}:{left:'140px'};
+		$colorImage.animate(colorImageParam,speed);
+		$item.animate(itemParam,speed);
+
 	};
+
 	init();
 
 
